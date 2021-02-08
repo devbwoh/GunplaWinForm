9303f0c77c8c92590593930df1353f1ca4758e77
bwoh <bwohkit@gmail.com>
2021-02-08 23:55:44 +0900

    🗃 데이터베이스 접속

--- GunplaWinForm/Form1.cs
+++ GunplaWinForm/Form1.cs
@@ -13,5 +13,18 @@ namespace GunplaWinForm {
         public Form1() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+            GunplaRepository db = new GunplaRepository();
+
+            string error = db.Connect();
+
+            if (error != null)
+                MessageBox.Show(error);
+            //else
+            //    MessageBox.Show("MySQL 접속 성공");
+
+            db.SelectMechanic();
+        }
     }
 }
---
+++ GunplaWinForm/GunplaRepository.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunplaWinForm {
+    class GunplaRepository {
+        MySqlConnection dbc = null;
+        MySqlDataReader res = null;
+
+        public string Connect() {
+            string con = "server=52.79.110.60;user=root;database=gunpladb;port=52804;password=mysql1234";
+            dbc = new MySqlConnection(con);
+            try {
+                dbc.Open();
+            }
+            catch (Exception ex) {
+                return ex.ToString();
+            }
+            return null;
+        }
+        public void Close() {
+            dbc.Close();
+        }
+
+        public void SelectMechanic() {
+            string query = "SELECT * FROM mechanic";
+            MySqlCommand cmd = new MySqlCommand(query, dbc);
+            res = cmd.ExecuteReader();
+
+            while (res.Read()) {
+                Console.WriteLine(res[0] + " -- " + res[1]);
+            }
+            res.Close();
+        }
+    }
+
+}
